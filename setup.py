865e6d1e0274eb73ebb948f0fd3cf072dd147ada
SirMax28 <samuelbernal310@gmail.com>
2025-02-26 17:45:32 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="relaseRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Samuel Steven",                         # Tu nombre
+    author_email="test@test.com",                 # Tu correo electrónico
+    url="https://github.com/sirmax28/relaserepo",     # URL del proyecto
+)
